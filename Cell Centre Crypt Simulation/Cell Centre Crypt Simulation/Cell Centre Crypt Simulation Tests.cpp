225c2e247d3908fa0fe07969c828e15717f1304f
TimInghamDempster <tim_ingham_dempster@hotmail.co.uk>
2015-12-16 15:05:13 +0000

    [TID] Set up build configs for unit tests.  Add the appropriate cpp files.

---
+++ Cell Centre Crypt Simulation/Cell Centre Crypt Simulation/Cell Centre Crypt Simulation Tests.cpp
@@ -0,0 +1,15 @@
+
+#include "SystemIncludes.cpp"
+
+#include "WindowsPlatform.cpp"
+
+#include "ProgramIncludes.cpp"
+
+#include "Renderer.cpp"
+
+#include "Tests/TestIncludes.cpp"
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
